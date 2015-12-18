00919cf8392f953418e066415e3ec646d0e0d0a3
mateusz <mateuszalmannai@googlemail.com>
2015-12-18 14:36:43 +0100

    add basic Handlebars expressions, localization, built-in each helper and a partial

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/*
+  create an immediately invoked function expression to keep
+  global scope clean
+*/
+(function(){
+
+  registerPartials();
+  renderPage();
+  renderDogs();
+
+  function registerPartials(){
+    Handlebars.registerPartial('dog', $('#dog-template').html());
+  }
+
+  function renderPage(){
+    var template = $('#index-template').html(),
+        compiled = Handlebars.compile(template),
+        rendered = compiled(window.language);
+    $('#main').html(rendered);
+    $('#languageSwitch').click(function () {
+      DogPack.switchLanguage();
+    });
+  }
+  function renderDogs(){
+    var template = $('#dogs-template').html(),
+      compiled = Handlebars.compile(template),
+      rendered = compiled({dogs: DogPack.dogs, language: window.language});
+    $('#theDogs').html(rendered);
+    attachDogButtons();
+  }
+
+  function attachDogButtons(){
+    $('.dog-button').click(function () {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      DogPack.chooseDog(id);
+      renderDogs();
+    });
+
+    $('.not-dog-button').click(function () {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      DogPack.chooseNotDog(id);
+      renderDogs();
+    });
+  }
+
+})();
